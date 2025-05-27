f609829090cb82dfae038e856867040bd98ee194
Sreerag <srrgsreekanth001@gmail.com>
2025-05-27 19:29:41 +0530

    updated password confirm

--- frontend/src/components/ResetPasswordConfirm.jsx
+++ frontend/src/components/ResetPasswordConfirm.jsx
@@ -25,7 +25,7 @@ const ResetPasswordConfirm = () => {
 
     try {
       await axios.post(
-        "http://localhost:8000/api/password-reset-confirm/",
+        '${process.env.REACT_APP_API_URL}/password-reset-confirm/',
         {
           uid,
           token,
